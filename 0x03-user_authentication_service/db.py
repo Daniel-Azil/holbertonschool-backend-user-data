650dc50b3c77c25179ba95414c17e8d9a191d144
Daniel Azil <danielazil.fse.dl@gmail.com>
2024-08-15 11:00:11 +0000

    Added comprehensive docstring for add_user method.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,13 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
+        """
+            A method that adds a new user to the database
+            Args:
+                email (str): Email address of the user
+                hashed_password (str): User's password hashed with byscript
+            Return:
+                User: A new user object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
